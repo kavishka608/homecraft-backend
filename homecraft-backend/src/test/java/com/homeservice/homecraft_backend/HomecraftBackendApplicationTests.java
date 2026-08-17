cd22650a08abf08e7cb6fde5df6f843593647b69
Kavishka Dewduni <kavishkadewduni@gmail.com>
2026-08-17 11:40:25 +0530

    Add files via upload

---
+++ homecraft-backend/src/test/java/com/homeservice/homecraft_backend/HomecraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeservice.homecraft_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecraftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
